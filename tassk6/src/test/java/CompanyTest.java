50770b62602900e72ed2055f4c38223855f99ac1
АлексейБутко <93659520+AlekseyButko@users.noreply.github.com>
2021-12-14 23:53:59 +0200

    Task6(remastered)
    Task6(remastered)

---
+++ tassk6/src/test/java/CompanyTest.java
@@ -0,0 +1,71 @@
+import com.company.Company;
+import com.company.Employee;
+import com.company.Manager;
+import com.company.SalesPerson;
+import org.junit.Test;
+import org.junit.*;
+
+
+import java.math.BigDecimal;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class CompanyTest {
+
+    @Test
+   public void Bonus() {
+        SalesPerson person1 = new SalesPerson("Саша", new BigDecimal(14_000), 150);
+        SalesPerson person2 = new SalesPerson("Маша", new BigDecimal(19_000), 210);
+        Manager person3 = new Manager("Вася", new BigDecimal(35_000), 120);
+
+        Company company = new Company(new Employee[]{
+                person1,
+                person2,
+                person3,
+        });
+
+        company.Bonus(new BigDecimal(1000));
+        assertEquals(new BigDecimal(1000), person1.getBonus());
+        assertEquals(new BigDecimal(1000), person2.getBonus());
+        assertEquals(new BigDecimal(1000), person3.getBonus());
+
+    }
+
+    @Test
+    public void Pay() {
+        SalesPerson person1 = new SalesPerson("Саша", new BigDecimal(10_000), 120);
+        SalesPerson person2 = new SalesPerson("Маша", new BigDecimal(15_000), 260);
+        Manager person3 = new Manager("Вася", new BigDecimal(25_000), 160);
+
+
+        Company company = new Company(new Employee[]{
+                person1,
+                person2,
+                person3,
+
+        });
+
+        assertEquals(person1.toPay().add(person2.toPay()).add(person3.toPay()), company.Pay());
+
+        company.Bonus(new BigDecimal(1000));
+
+        assertEquals(person1.toPay().add(person2.toPay()).add(person3.toPay()), company.Pay());
+    }
+
+    @Test
+    public void Wage() {
+        SalesPerson person1 = new SalesPerson("Саша", new BigDecimal(10_000), 120);
+        SalesPerson person2 = new SalesPerson("Маша", new BigDecimal(15_000), 260);
+        Manager person3 = new Manager("Вася", new BigDecimal(25_000), 160);
+
+        Company company = new Company(new Employee[]{
+                person1,
+                person2,
+                person3,
+        });
+
+        String name = company.Salary();
+        assertEquals(person3.getName(), name);
+    }
+}
